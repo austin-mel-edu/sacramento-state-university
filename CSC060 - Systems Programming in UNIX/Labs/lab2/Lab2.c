ea547680dc0d73d1dc706a8af65c52d3e8d8172f
austin-mel-edu <austin.csus@gmail.com>
2023-09-19 12:25:15 -0700

    Add csc60 lab one files

---
+++ CSC060 - Systems Programming in UNIX/Labs/lab2/Lab2.c
@@ -0,0 +1,17 @@
+/*---------------*/
+/*Austin Melendez*/
+/*Lab 2          */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("\nLab 2. \n\n");
+    printf("Hello World.\n");
+    printf("Hi, Austin Melendez. \n\n");
+    printf("People change and things go wrong, \n");
+    printf("but just remember, life goes on. \n");
+    printf("- Mac Miller \n\n");
+    return EXIT_SUCCESS;
+}
+/*---------------*/
