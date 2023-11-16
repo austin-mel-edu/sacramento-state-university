f660847dc04ae62cde397a10aec307fa0f8f4fe9
austin-mel-edu <austin.csus@gmail.com>
2023-11-16 11:26:50 -0800

    Add csc60 lab nine files

---
+++ CSC060 - Systems Programming in UNIX/Labs/lab9/ParseCmd.c
@@ -0,0 +1,26 @@
+/* ----------------------------------------------------------------- */
+/*                  ParseCmd                                        */
+/* ----------------------------------------------------------------- */
+/* Parse the input line into argc/argv format */
+
+#include "lab9_10.h"
+
+int ParseCmd(char *cmdline, char **argv)
+{
+    int argc = 0;
+    char *separator = " \n\t";  /* Includes space, Enter, Tab */
+
+    /* strtok searches for the characters listed in "separator" */
+    /* Find the first token */ 
+    argv[argc] = strtok(cmdline, separator);
+ 
+    /* Find the next token & check for end  of line */
+    while ((argv[argc] != NULL) && (argc+1 < MAXARGS))
+    { 
+        argv[++argc] = strtok((char *) 0, separator);
+    }
+    return argc;
+}
+/* end of function ParseCmd */
+
+/* ----------------------------------------------------------------- */
